cc6fe19a68bdce3a8f5b415018694fb660f2a09c
Navruzq <qarshiyevnavruz7777@gmail.com>
2022-11-04 19:34:37 +0300

    1

--- For01.py
+++ For01.py
@@ -10,10 +10,11 @@ def main(n):
         list: return  answer
     """
     i=0
-    a=list1=[]
-    while i<n:
+    list1=[]
+    for i in range(n+1):
         list1=list1+[i]
-        i+=1
     return list1
 print(main(5))
+
+
         
